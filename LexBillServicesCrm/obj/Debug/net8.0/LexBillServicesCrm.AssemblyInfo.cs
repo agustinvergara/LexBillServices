788f214f1c83489095c0619d12445244f91729f4
agustinvergara <agustin.vergara.ruiz@gmail.com>
2024-07-15 11:13:45 -0500

    Guardar cambios antes del merge

--- LexBillServicesCrm/obj/Debug/net8.0/LexBillServicesCrm.AssemblyInfo.cs
+++ LexBillServicesCrm/obj/Debug/net8.0/LexBillServicesCrm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LexBillServicesCrm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8fcadd23eff37aff05b3d811ef8fbea3ca4e1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5238e2ee628113953bd3e3e5edb1dc38996a3153")]
 [assembly: System.Reflection.AssemblyProductAttribute("LexBillServicesCrm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LexBillServicesCrm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
